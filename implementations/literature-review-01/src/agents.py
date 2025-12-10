48baf03f7f9932c4cf118ad3afab817e0bf161d6
Amanda da Silveira <amanda.s.silveira@outlook.com>
2025-12-10 04:28:19 -0300

    agent builders - literature review 1

---
+++ implementations/literature-review-01/src/agents.py
@@ -0,0 +1,91 @@
+"""
+Agent creation and configuration.
+Each agent has a specific role in the literature review pipeline.
+"""
+
+from autogen_agentchat.agents import AssistantAgent
+from src.tools import Search_arXiv
+from src.prompts import RESEARCHER_PROMPT, REVIEWER_PROMPT, WRITER_PROMPT
+from src.config import ModelConfig, AgentConfig
+from autogen_ext.models.ollama import OllamaChatCompletionClient
+
+
+
+ollama_client_lamma = OllamaChatCompletionClient(
+    model=ModelConfig.LLAMA_3_1,
+    
+    follow_redirects=False,
+)
+
+ollama_client_granite = OllamaChatCompletionClient(
+            model="granite3.3:8b",
+            model_info= ModelConfig.granite_capabilities
+        )
+
+
+def create_researcher_agent() -> AssistantAgent:
+    """
+    Create the Researcher agent.
+    
+    This agent is responsible for:
+    - Understanding the research topic
+    - Formulating effective search queries
+    - Using the arxiv_search tool to find papers
+    - Returning structured paper information
+    
+    Returns:
+        AssistantAgent configured as a researcher
+    """
+    return AssistantAgent(
+        name=AgentConfig.RESEARCHER_NAME,
+        description=AgentConfig.RESEARCHER_DESCRIPTION,
+        model_client=ollama_client_lamma,
+        tools=[Search_arXiv],  # Give it access to arXiv search
+        reflect_on_tool_use=False,
+        model_client_stream=True,
+        system_message=RESEARCHER_PROMPT
+    )
+
+def create_reviewer_agent() -> AssistantAgent:
+    """
+    Create the Reviewer agent.
+    
+    This agent is responsible for:
+    - Evaluating paper relevance to the user's query
+    - Checking coverage of key topics
+    - Identifying gaps or off-topic papers
+    - Validating the paper selection
+    
+    Returns:
+        AssistantAgent configured as a reviewer
+    """
+    return AssistantAgent(
+        name=AgentConfig.REVIEWER_NAME,
+        description= AgentConfig.REVIEWER_DESCRIPTION,
+        model_client=ollama_client_granite,
+        model_client_stream=True,
+        system_message=REVIEWER_PROMPT
+    )
+
+def create_writer_agent() -> AssistantAgent:
+    """
+    Create the Writer agent.
+    
+    This agent is responsible for:
+    - Synthesizing information from multiple papers
+    - Creating a structured literature review
+    - Comparing and contrasting different approaches
+    - Identifying trends and future directions
+    
+    Returns:
+        AssistantAgent configured as an academic writer
+    """
+    return AssistantAgent(
+        name=AgentConfig.WRITER_NAME,
+        description= AgentConfig.WRITER_DESCRIPTION,
+        model_client=ollama_client_granite,
+        model_client_stream=True,
+        system_message=WRITER_PROMPT
+    )
+
+
